95f7f08ebc1379aac2bf326826c1bdf4b105e4d0
Saloni Gupta <70319586+saloni24gupta@users.noreply.github.com>
2024-05-04 16:48:07 +0530

    useRef Task

--- 01-starting-project/src/components/Player.jsx
+++ 01-starting-project/src/components/Player.jsx
@@ -1,21 +1,23 @@
-import React, {useState} from 'react'
 
+import React, {useState, useRef} from 'react'
+// step 1 =>  import useRef
 function Player() {
     const [enteredPlayerName, setEnterPlayerName] = useState('');
- const [submitted, setSubmitted] = useState(false)
- function handleChange(e) {
-    setSubmitted(false);
-    setEnterPlayerName(e.target.value);
-    console.log(enteredPlayerName)
-    }
+    // step 2 => declear useRef fun value
+const playerName = useRef()
+//  const [submitted, setSubmitted] = useState(false)
+
     function handleClick() {
-        setSubmitted(true);
+        // setSubmitted(true);
+        // step 4 => setPlayervalue => playername,current.value
+        setEnterPlayerName(playerName.current.value)
     }
     return (
     <div>
 
-        <h2>Welcome {submitted ? enteredPlayerName : 'unknown entity'}</h2>
-        <input type="text" placeholder='Enter Name'  onChange={handleChange} value={enteredPlayerName}/>
+        <h2>Welcome {enteredPlayerName ? enteredPlayerName : 'unknown entity'}</h2>
+        {/* step 3 => define ref={value} */}
+        <input type="text"  ref={playerName} />
         <button onClick={handleClick}>set Name</button>
     </div>
   )
